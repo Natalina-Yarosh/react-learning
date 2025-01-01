a503b1afe605d16d91df3401a3ca1a4746ed35ae
Farid Mahmudov <Razormad666@gmail.com>
2025-01-01 22:27:17 +0300

    update

--- src/hooks/service/useTheme.ts
+++ src/hooks/service/useTheme.ts
@@ -1,27 +1,23 @@
 import { useState, useEffect } from "react"
 
+ 
   /**
-   * useTheme returns the current theme color and a function to set the theme color
-   * The theme color is determined by the theme parameter in the URL
-   * The default theme color is #333 (dark)
-   * The theme color is used to set the className of the body element to "theme-<color>"
-   * @returns {[string, (color: string) => void]} - The theme color and a function to set the theme color
+   * Hook to get the current theme color based on the url parameters.
+   * @returns an object with the themeColor property.
    * @example
-   * const [themeColor, setThemeColor] = useTheme()
-   * // themeColor is '#333'
-   * setThemeColor('light')
-   * // themeColor is 'ghostwhite'
-   * // document.body.className is 'theme-ghostwhite'
+   * const { themeColor } = useTheme()
+   * // themeColor is '#111' if the url parameter is 'dark'
+   * // or 'ghostwhite' if the url parameter is 'light'
    */
 export const useTheme = () => {
 
-  const [themeColor, setThemeColor] = useState('#333')
   const [urlParams, _] = useState(new URLSearchParams(window.location.search))
+  const [themeColor, setThemeColor] = useState('ghostwhite')
 
 
   useEffect(() => {
     if(urlParams.get('theme') === 'dark') {
-      setThemeColor('#333')
+      setThemeColor('#111')
     }
     else if(urlParams.get('theme') === 'light') {
       setThemeColor('ghostwhite')
